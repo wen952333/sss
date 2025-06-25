4949c9fc2ea8b7332918f98e0b0c6581829c315c
wen952333 <wenxiu9523@outlook.com>
2025-06-25 22:46:06 +0800

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://9526.ip-ddns.com/api.php"; // PHP后端入口
+const API_BASE = "https://ss.wenge.ip-ddns.com/api.php"; // PHP后端入口
 
 export async function apiRequest(action, data = {}) {
   // 统一POST请求
