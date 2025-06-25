7c37524f918f2df5e3cc310234e5ad50274e9608
wen952333 <wenxiu9523@outlook.com>
2025-06-25 22:03:11 +0800

    Create config.php

---
+++ backend/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'your_db_user');
+define('DB_PASS', 'your_db_pass');
+define('DB_NAME', 'your_db_name');
+header("Access-Control-Allow-Origin: https://ss.wenge.ip-ddns.com");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type");
+?>
