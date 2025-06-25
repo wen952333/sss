f6307207d3ef5598198dec4dda7d7d2dbd4e1926
wen952333 <wenxiu9523@outlook.com>
2025-06-25 15:32:07 +0800

    Create index.js

---
+++ frontend/src/api/index.js
@@ -0,0 +1,17 @@
+const API_BASE = "https://9526.ip-ddns.com/api";
+
+export async function apiPost(endpoint, data) {
+  const res = await fetch(`${API_BASE}/${endpoint}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+  return await res.json();
+}
+
+export async function apiGet(endpoint, params = {}) {
+  const url = new URL(`${API_BASE}/${endpoint}`);
+  Object.entries(params).forEach(([k, v]) => url.searchParams.append(k, v));
+  const res = await fetch(url, { credentials: "include" });
+  return await res.json();
+}
