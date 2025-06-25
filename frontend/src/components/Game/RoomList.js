67a5366eee059ea35b9d47d950dac2d66b81f6d3
wen952333 <wenxiu9523@outlook.com>
2025-06-25 16:36:01 +0800

    Create RoomList.js

---
+++ frontend/src/components/Game/RoomList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { apiRequest } from '../../api';
+
+export default function RoomList({ onJoinRoom, user }) {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const res = await apiRequest('room_list', {});
+      if (res.success) setRooms(res.rooms);
+    };
+    fetchRooms();
+    const interval = setInterval(fetchRooms, 2000); // 轮询房间列表
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h2 className="font-bold text-lg">房间列表</h2>
+      <ul>
+        {rooms.map(r => (
+          <li key={r.id} className="flex justify-between py-2">
+            <span>{r.name}（{r.players.length}人）</span>
+            <button onClick={() => onJoinRoom(r.id)} className="btn">加入</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
