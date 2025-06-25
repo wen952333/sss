9cddd17162909d82c6f522f5c95d58faf84044c3
wen952333 <wenxiu9523@outlook.com>
2025-06-25 16:38:35 +0800

    Delete backend directory

--- backend/utils.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getUserByToken($token) {
-  global $db;
-  if (!$token) return null;
-  $user = $db->query("SELECT * FROM users WHERE token=?", [$token])->fetch();
-  return $user ?: null;
-}
