664524ab550c759563dd0ab14f160ec47a99adb3
wen952333 <wenxiu9523@outlook.com>
2025-06-26 15:59:20 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api.php': {
-        target: 'https://9526.ip-ddns.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    host: '0.0.0.0',
+    port: 5173,
   },
+  base: './',
 });
