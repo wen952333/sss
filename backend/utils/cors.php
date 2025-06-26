2e8bc7d11f0c55f834c44097764175462b41a336
wen952333 <wenxiu9523@outlook.com>
2025-06-26 16:24:07 +0800

    Create cors.php

---
+++ backend/utils/cors.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: https://ss.wenge.ip-ddns.com");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
