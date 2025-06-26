c35af641f0e13fa1be52f870838925fc3b902eb0
wen952333 <wenxiu9523@outlook.com>
2025-06-26 17:10:22 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Room from './pages/Room';
 import Play from './pages/Play';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/room/:roomId" element={<Room />} />
         <Route path="/play/:roomId" element={<Play />} />
       </Routes>
