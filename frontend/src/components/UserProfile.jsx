0defcb718c42db48c01fe0ea5f7a42fc44b9498e
wen952333 <wenxiu9523@outlook.com>
2025-06-26 13:23:27 +0800

    Delete frontend directory

--- frontend/src/components/UserProfile.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function UserProfile({ user, setUser }) {
-  return (
-    <div className="user-profile">
-      <span>你好，{user.nickname}（{user.phone}） | 积分：{user.score}</span>
-      <button onClick={() => setUser(null)}>退出登录</button>
-    </div>
-  );
-}
