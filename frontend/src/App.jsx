8b9fe420b8c192462d853b5d6dfa588e8df3ad09
wen952333 <wenxiu9523@outlook.com>
2025-06-27 18:55:04 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Room from './pages/Room';
 import Play from './pages/Play';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import TryPlay from './pages/TryPlay';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/room/:roomId" element={<Room />} />
         <Route path="/play/:roomId" element={<Play />} />
+        <Route path="/try" element={<TryPlay />} />
       </Routes>
     </BrowserRouter>
   );
