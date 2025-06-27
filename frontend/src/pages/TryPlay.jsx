83b2f2fc5ca191bfb70a23a3f2995c29d1279fd6
wen952333 <wenxiu9523@outlook.com>
2025-06-27 22:28:27 +0800

    Update TryPlay.jsx

--- frontend/src/pages/TryPlay.jsx
+++ frontend/src/pages/TryPlay.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import CompareResultModal from '../pages/CompareResultModal';
 import { getSmartSplits } from './SmartSplit';
+import CompareResultModal from '../pages/CompareResultModal';
 import './Play.css';
 
 const allSuits = ['clubs', 'spades', 'diamonds', 'hearts'];
